74c6b38ab699ef105417453da789659d2a93cf59
Jonathan Herber <jonathanherber15@gmail.com>
2022-04-05 13:36:11 -0300

    CORS

--- routes/routes.go
+++ routes/routes.go
@@ -4,17 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/jonathanherber/Golang_api/controllers"
+	"github.com/jonathanherber/Golang_api/middleware"
 )
 
 func HandleRequest() {
-	r := mux.NewRouter() //instacia do gorilla mux, que é um router de url
+	r := mux.NewRouter() //instancia do gorilla mux, que é um router de url
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("GET")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaPersonalidade).Methods("GET")
 	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("POST")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("DELETE")
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditaUmaPersonalidade).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
