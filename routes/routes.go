8ceeb5eefac05a320fc3e07615c69cfcc60128bb
Jonathan Herber <jonathanherber15@gmail.com>
2022-04-05 11:24:13 -0300

    Https verbs

--- routes/routes.go
+++ routes/routes.go
@@ -9,9 +9,12 @@ import (
 )
 
 func HandleRequest() {
-	r := mux.NewRouter() //instacia do gorilla mux
+	r := mux.NewRouter() //instacia do gorilla mux, que é um router de url
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("GET")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaPersonalidade).Methods("GET")
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("POST")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("DELETE")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaUmaPersonalidade).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
